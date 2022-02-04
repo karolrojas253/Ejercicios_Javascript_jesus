905358790cc84d0dd2f917ce8b9cb9a11a71aae4
karolrojas253 <karolxiomararojassoriano@gmail.com>
2022-02-04 11:06:40 -0500

    saludo

---
+++ js/domicilio.js
@@ -0,0 +1,9 @@
+"use strict"
+
+//CALCULAR COSTO TOTAL, INCLUYENDO IMPUESTOS Y DOMICILIO.
+
+let valorPorducto = 20000;
+let impuesto = .10;
+let domicilio = 2000;
+ 
+console.log("El costo total es de: $", ((valorPorducto * impuesto) + valorPorducto  +  domicilio));
